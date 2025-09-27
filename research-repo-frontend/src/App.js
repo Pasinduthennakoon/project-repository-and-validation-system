1ac24e9d1ffad008a123a7303e27d5d062315e21
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-09-28 00:10:01 +0530

    create react app

---
+++ research-repo-frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-5xl text-blue-600 font-bold underline">
+      Hello world!
+    </h1>
+  );
+}
+
+export default App;
