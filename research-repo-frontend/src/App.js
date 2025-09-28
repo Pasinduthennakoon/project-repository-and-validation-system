b0aa7ee10822785e9a2979d6dab5feac067e84e2
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-09-28 22:44:01 +0530

    update

--- research-repo-frontend/src/App.js
+++ research-repo-frontend/src/App.js
@@ -1,11 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Pages/Home";
+import Projects from "./Pages/Projects";
+import Upload from "./Pages/Upload";
+import ProjectDetails from "./Pages/ProjectDetails";
+import Navigation from "./components/Navigation/Navigation";
+import Footer from "./components/Footer/Footer";
+
 
 function App() {
   return (
-    <h1 className="text-5xl text-blue-600 font-bold underline">
-      Hello world!
-    </h1>
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <div className="Navigation">
+          <Navigation/>
+        </div>
+        
+        <div className=" ">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/upload" element={<Upload />} />
+            <Route path="/projects/:id" element={<ProjectDetails />} />
+          </Routes>
+        </div>
+
+      </div>
+      <div className="Footer">
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 
