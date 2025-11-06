95a891a1066701b40e55485a4776e42ae82f0b30
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-11-06 21:13:56 +0530

    Update App.js

--- research-repo-frontend/src/App.js
+++ research-repo-frontend/src/App.js
@@ -1,38 +1,31 @@
 // src/App.js
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./Pages/Home";
-import Projects from "./Pages/Projects";
-import Upload from "./Pages/Upload";
-import ProjectDetails from "./Pages/ProjectDetails";
+import { BrowserRouter as Router } from "react-router-dom";
 import Navigation from "./components/Navigation/Navigation";
 import Footer from "./components/Footer/Footer";
-import IdeaComparisonPage from "./Pages/IdeaComparisonPage";
 import { footerContent } from "./data/footerContent";
+import Routers from "./routers/routers";
+import { AuthProvider } from "./context/AuthContext";
 
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-50">
-        <div className="Navigation">
-          <Navigation/>
-        </div>
-        
-        <div className=" ">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/upload" element={<Upload />} />
-            <Route path="/projects/:id" element={<ProjectDetails />} />
-            <Route path="/ideavalidation" element={<IdeaComparisonPage />} />
-          </Routes>
-        </div>
+      <AuthProvider>
+        <div className="min-h-screen bg-gray-50">
+          <div className="Navigation">
+            <Navigation />
+          </div>
 
-      </div>
-      <div className="Footer">
-        <Footer content={footerContent}/>
-      </div>
+          <div className=" ">
+            <Routers />
+          </div>
+
+        </div>
+        <div className="Footer">
+          <Footer content={footerContent} />
+        </div>
+      </AuthProvider>
     </Router>
   );
 }
