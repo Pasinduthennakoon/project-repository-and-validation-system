e762a9ac0536ce0f1ec8048d0f17859393931eb0
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-11-08 14:24:42 +0530

    update

--- research-repo-frontend/tailwind.config.js
+++ research-repo-frontend/tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // This will replace the default sans-serif stack
+        sans: ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', '"Noto Sans"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
+      },
+    },
   },
   plugins: [],
 }
