cf28a9e0ed748bb6feeb920a75c0907dfd5dc077
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-11-09 10:14:43 +0530

    Update StudentDashboard.jsx

--- research-repo-frontend/src/Pages/StudentDashboard.jsx
+++ research-repo-frontend/src/Pages/StudentDashboard.jsx
@@ -6,9 +6,61 @@ import DepartmentBarChart from "../components/student/Charts/DepartmentBarChart"
 import YearTrendChart from "../components/student/Charts/YearTrendChart";
 import WordCloudChart from "../components/student/Charts/WordCloudChart";
 import UnexploredAreas from "../components/student/Charts/UnexploredAreas";
+import TrendLineChart from "../components/admin/Charts/TrendLineChart";
+import GapAnalysisSection from "../components/admin/GapAnalysisSection";
 
 const StudentDashboard = () => {
   const projects = sampleProjects;
+
+  const trendData = {
+    labels: ["2021", "2022", "2023", "2024", "2025"],
+    datasets: [
+      {
+        label: "AI Projects",
+        data: [5, 12, 19, 28, 37],
+        borderColor: "#10b981",
+        fill: false,
+      },
+      {
+        label: "Cybersecurity Projects",
+        data: [10, 23, 20, 12, 5],
+        borderColor: "#10b981",
+        fill: false,
+      },
+      {
+        label: "blockchain Projects",
+        data: [2, 15, 20, 10, 3],
+        borderColor: "#10b981",
+        fill: false,
+      },
+      {
+        label: "Iot Projects",
+        data: [0, 2, 10, 2, 15],
+        borderColor: "#10b981",
+        fill: false,
+      },
+      {
+        label: "ML Projects",
+        data: [0, 15, 10, 30, 35],
+        borderColor: "#10b981",
+        fill: false,
+      },
+      {
+        label: "Security Projects",
+        data: [0, 6, 10, 12, 5],
+        borderColor: "#10b981",
+        fill: false,
+      },
+    ],
+  };
+
+  const isValidChartData = (data) =>
+    data &&
+    Array.isArray(data.labels) &&
+    data.labels.length > 0 &&
+    Array.isArray(data.datasets) &&
+    data.datasets.length > 0 &&
+    Array.isArray(data.datasets[0].data);
 
   return (
     <div className="p-10 space-y-10">
@@ -18,11 +70,11 @@ const StudentDashboard = () => {
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         <CategoryPieChart projects={projects} />
-        <DepartmentBarChart projects={projects} />
-        <YearTrendChart projects={projects} />
         <WordCloudChart projects={projects} />
+          {isValidChartData(trendData) && <TrendLineChart data={trendData} />}
       </div>
 
+      <GapAnalysisSection />
       <UnexploredAreas projects={projects} />
     </div>
   );
