116f6c571cdd897f43a9acdf00dc44afdd534c9c
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-11-18 22:50:59 +0530

    Update HeroSection.jsx

--- research-repo-frontend/src/components/HeroSection/HeroSection.jsx
+++ research-repo-frontend/src/components/HeroSection/HeroSection.jsx
@@ -3,23 +3,29 @@ import HeroImg from '../../assets/img/Research_banner.jpeg';
 
 const HeroSection = () => {
   return (
-    <div className='relative flex items-center bg-cover flext-start bg-cover bg-center text-left h-[600px] w-full' style={{backgroundImage
-    : `url(${HeroImg})`}}>
-        <div className='absolute top-0 right-0 bottom-0 left-0'></div>
-            <main className='px-10 lg:px-24 z-10'>
-                <div className='text-left'>
-                    <h2 className='text-2xl text-white'>Welcome to</h2>
-                </div>
-                <p className='mt-3 text-white sm:mt-5 sm:max-w-xl text-6xl'>
-                Research Repo
-                </p>
-                <p className='mt-3 text-white sm:mt-5 sm:max-w-xl text-2xl'>
-                Browse | upload | explore student research projects.
-                </p>
-            </main>
-        
-    </div>
-  )
-}
+    <div
+      className="relative flex items-center bg-cover bg-center text-left h-[600px] w-full z-0"
+      style={{ backgroundImage: `url(${HeroImg})` }}
+    >
+      {/* Overlay */}
+      <div className="absolute inset-0 z-0"></div>
 
-export default HeroSection
+      {/* Content */}
+      <main className="px-10 lg:px-24 relative z-0">
+        <div className="text-left">
+          <h2 className="text-2xl text-white">Welcome to</h2>
+        </div>
+
+        <p className="mt-3 text-white sm:mt-5 sm:max-w-xl text-6xl">
+          Research Repo
+        </p>
+
+        <p className="mt-3 text-white sm:mt-5 sm:max-w-xl text-2xl">
+          Browse | upload | explore student research projects.
+        </p>
+      </main>
+    </div>
+  );
+};
+
+export default HeroSection;
