8ab7a0a3d3ea589f2d6f0ac62b3375adcf77b7d7
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-11-18 22:57:20 +0530

    Create UserSevice.java

---
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/service/UserSevice.java
@@ -0,0 +1,10 @@
+package com.trinco.researchrepo.research_repo_backend.service;
+
+import com.trinco.researchrepo.research_repo_backend.dto.request.PendingUserSaveRequestDTO;
+import com.trinco.researchrepo.research_repo_backend.dto.request.UserSaveRequestDTO;
+import com.trinco.researchrepo.research_repo_backend.entity.Users;
+import org.springframework.stereotype.Service;
+
+public interface UserSevice {
+    Users addUser(UserSaveRequestDTO userSaveRequestDTO);
+}
