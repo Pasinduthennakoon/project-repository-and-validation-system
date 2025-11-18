f1a78e68c4c3fe3ef8ffb9a889d10ea5aec993ba
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-11-18 22:56:45 +0530

    Create UserSaveRequestDTO.java

---
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/dto/request/UserSaveRequestDTO.java
@@ -0,0 +1,31 @@
+package com.trinco.researchrepo.research_repo_backend.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class UserSaveRequestDTO {
+
+    private String userName;
+    private String department;
+    private String email;
+    private String role;        // e.g., "STUDENT", "TEACHER"
+    private String password;
+    private String photoLink;
+    private boolean activeState;
+
+    private StudentSaveRequestDTO studentInfo;
+
+    public UserSaveRequestDTO(String userName, String department, String email, String role, String password, String photoLink, boolean activeState) {
+        this.userName = userName;
+        this.department = department;
+        this.email = email;
+        this.role = role;
+        this.password = password;
+        this.photoLink = photoLink;
+        this.activeState = activeState;
+    }
+}
