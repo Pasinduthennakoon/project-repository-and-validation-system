083710a09685176df8303fe82012f89883c98f41
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-11-18 22:58:12 +0530

    Create UserRepo.java

---
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/repo/UserRepo.java
@@ -0,0 +1,14 @@
+package com.trinco.researchrepo.research_repo_backend.repo;
+
+import com.trinco.researchrepo.research_repo_backend.entity.Users;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+@EnableJpaRepositories
+@Repository
+@Transactional
+public interface UserRepo extends JpaRepository<Users, Integer> {
+    boolean existsByEmail(String email);
+}
