1fdb81eced581c9d0bd62d73d8e3fbaba1506071
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-11-22 22:54:54 +0530

    Create Pending_Projects.java

---
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/entity/Pending_Projects.java
@@ -0,0 +1,49 @@
+package com.trinco.researchrepo.research_repo_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "pending_projects")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Pending_Projects {
+
+    @Id
+    @Column(name = "pending_project_id",length = 45)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int pendingProjectId;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "abstract",nullable = false)
+    private String abstract_;
+
+    @Column(name = "github_link")
+    private String githubLink;
+
+    @Column(name = "temp_pdf_path",nullable = false)
+    private String tempPdfPath;
+
+    @Column(name = "department",nullable = false)
+    private String department;
+
+    @Column(name = "uploader_id",nullable = false)
+    private int uploaderId;
+
+    @Column(name = "batch",nullable = false)
+    private String batch;
+
+    @Column(name = "supervisor_id",nullable = false)
+    private int supervisorId;
+
+    @Column(name = "created_at")
+    private String createdAt;
+
+}
