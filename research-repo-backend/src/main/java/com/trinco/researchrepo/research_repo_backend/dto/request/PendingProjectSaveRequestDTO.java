22929ba1f7838ca03a616120aa37fae04357151b
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-11-22 22:54:43 +0530

    Create PendingProjectSaveRequestDTO.java

---
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/dto/request/PendingProjectSaveRequestDTO.java
@@ -0,0 +1,23 @@
+package com.trinco.researchrepo.research_repo_backend.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class PendingProjectSaveRequestDTO {
+
+    private String title;
+    private String abstract_;
+    private String githubLink;
+    private String tempPdfPath;
+    private String department;
+    private int uploaderId;
+    private String batch;
+    private int supervisorId;
+    private String createdAt;
+}
