3d9358ce6c32493d72297e9ce4b0ff3a2ba00bf1
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-11-23 22:33:23 +0530

    Create GoogleDriveServiceIMPL.java

---
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/service/impl/GoogleDriveServiceIMPL.java
@@ -0,0 +1,65 @@
+package com.trinco.researchrepo.research_repo_backend.service.impl;
+
+import com.trinco.researchrepo.research_repo_backend.service.GoogleDriveService;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.FileContent;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.io.*;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class GoogleDriveServiceIMPL implements GoogleDriveService {
+
+    private static final String APP_NAME = "ResearchRepo";
+    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+    private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE_FILE);
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+
+    private Credential getCredentials(final NetHttpTransport transport) throws IOException {
+        InputStream in = new FileInputStream("src/main/resources/client_secret.json");
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                transport, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
+
+    public String uploadFileToDrive(java.io.File file) throws IOException, GeneralSecurityException {
+        final NetHttpTransport transport = GoogleNetHttpTransport.newTrustedTransport();
+        Drive service = new Drive.Builder(transport, JSON_FACTORY, getCredentials(transport))
+                .setApplicationName(APP_NAME)
+                .build();
+
+        File fileMetadata = new File();
+        fileMetadata.setName(file.getName());
+
+        FileContent mediaContent = new FileContent("application/pdf", file);
+        File uploadedFile = service.files().create(fileMetadata, mediaContent)
+                .setFields("id")
+                .execute();
+
+        return "https://drive.google.com/uc?id=" + uploadedFile.getId();
+    }
+}
+
