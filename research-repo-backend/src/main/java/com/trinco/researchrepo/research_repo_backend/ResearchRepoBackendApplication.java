683d7aa8a7b11f33c67a60026bf1cf1328e87e53
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-12-03 18:37:03 +0530

    update

---
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/ResearchRepoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.trinco.researchrepo.research_repo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchRepoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchRepoBackendApplication.class, args);
+	}
+
+}
