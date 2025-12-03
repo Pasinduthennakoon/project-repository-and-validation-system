d7d4b7c8e10ccd7e0e84039592197e832ea108b1
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-12-03 18:36:05 +0530

    Update UsersMapper.java

--- research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/util/mappers/UsersMapper.java
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/util/mappers/UsersMapper.java
@@ -1,9 +1,12 @@
 package com.trinco.researchrepo.research_repo_backend.util.mappers;
 
+import com.trinco.researchrepo.research_repo_backend.dto.request.PendingUserSaveRequestDTO;
 import com.trinco.researchrepo.research_repo_backend.dto.request.StudentSaveRequestDTO;
 import com.trinco.researchrepo.research_repo_backend.dto.request.UserSaveRequestDTO;
+import com.trinco.researchrepo.research_repo_backend.entity.Pending_Users;
 import com.trinco.researchrepo.research_repo_backend.entity.Users;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UsersMapper {
@@ -22,4 +25,11 @@ public interface UsersMapper {
                 studentSaveRequestDTO.isActiveState()
         );
     }
+
+    PendingUserSaveRequestDTO EntityToPenddingUserDto(Pending_Users pending_Users);
+
+    @Mapping(target = "photoLink", ignore = true)
+    @Mapping(target = "activeState", ignore = true)
+    @Mapping(target = "studentInfo", ignore = true)
+    UserSaveRequestDTO PendingUserDtoToUserDto(PendingUserSaveRequestDTO pendingUserSaveRequestDTO);
 }
