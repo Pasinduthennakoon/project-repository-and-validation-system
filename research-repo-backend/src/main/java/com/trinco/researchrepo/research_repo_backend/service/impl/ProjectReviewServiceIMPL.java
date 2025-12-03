28e3d5e357ce5813b20cb68bd6e23821c656c965
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-12-03 22:51:25 +0530

    Create ProjectReviewServiceIMPL.java

---
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/service/impl/ProjectReviewServiceIMPL.java
@@ -0,0 +1,26 @@
+package com.trinco.researchrepo.research_repo_backend.service.impl;
+
+import com.trinco.researchrepo.research_repo_backend.entity.Reviews;
+import com.trinco.researchrepo.research_repo_backend.repo.ProjectReviewRepo;
+import com.trinco.researchrepo.research_repo_backend.service.ProjectReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectReviewServiceIMPL implements ProjectReviewService {
+
+    @Autowired
+    private ProjectReviewRepo projectReviewRepo;
+
+    @Override
+    public String updateStars(int projectId, int stars) {
+
+        if(projectReviewRepo.existsById(projectId)){
+            Reviews reviews = projectReviewRepo.getReferenceById(projectId);
+            reviews.setStars(reviews.getStars() + stars);
+            reviews.setRated(reviews.getRated() + 1);
+            projectReviewRepo.save(reviews);
+        }
+        return "Stars updated successfully";
+    }
+}
