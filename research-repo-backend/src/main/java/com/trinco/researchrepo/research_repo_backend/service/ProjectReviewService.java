fad6a46bb51be230856b14077abb4d259289619e
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-12-05 01:33:25 +0530

    Update ProjectReviewService.java

--- research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/service/ProjectReviewService.java
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/service/ProjectReviewService.java
@@ -3,7 +3,6 @@ package com.trinco.researchrepo.research_repo_backend.service;
 import com.trinco.researchrepo.research_repo_backend.dto.request.CommentSaveRequestDTO;
 
 public interface ProjectReviewService {
-    String updateStars(int projectId, int stars);
 
     String updateWatches(int projectId);
 }
