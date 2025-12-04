6e13c45e72564f3abb779f33aea13b7556bfffd3
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-12-05 01:33:28 +0530

    Update ProjectService.java

--- research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/service/ProjectService.java
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/service/ProjectService.java
@@ -1,7 +1,10 @@
 package com.trinco.researchrepo.research_repo_backend.service;
 
 import com.trinco.researchrepo.research_repo_backend.dto.request.ProjectSaveRequestDTO;
+import com.trinco.researchrepo.research_repo_backend.dto.response.ProjectPageDataResponseDTO;
 
 public interface ProjectService {
     String approveProject(int pendingId);
+
+    ProjectPageDataResponseDTO getProjectPageData(int projectId);
 }
