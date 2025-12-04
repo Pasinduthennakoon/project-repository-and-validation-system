fb3c248bf658b0fba582a33e86d3d3cc02dd5ac1
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-12-04 09:30:06 +0530

    Create CommentService.java

---
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/service/CommentService.java
@@ -0,0 +1,7 @@
+package com.trinco.researchrepo.research_repo_backend.service;
+
+import com.trinco.researchrepo.research_repo_backend.dto.request.CommentSaveRequestDTO;
+
+public interface CommentService {
+    String addComment(CommentSaveRequestDTO commentSaveRequestDTO);
+}
