6e4f11e62c5bc45a505ef52b8f0fdb280d2bec2e
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-12-05 01:33:12 +0530

    Update CommentServiceIMPL.java

--- research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/service/impl/CommentServiceIMPL.java
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/service/impl/CommentServiceIMPL.java
@@ -2,7 +2,9 @@ package com.trinco.researchrepo.research_repo_backend.service.impl;
 
 import com.trinco.researchrepo.research_repo_backend.dto.request.CommentSaveRequestDTO;
 import com.trinco.researchrepo.research_repo_backend.entity.Comments;
+import com.trinco.researchrepo.research_repo_backend.entity.Reviews;
 import com.trinco.researchrepo.research_repo_backend.repo.CommentRepo;
+import com.trinco.researchrepo.research_repo_backend.repo.ProjectReviewRepo;
 import com.trinco.researchrepo.research_repo_backend.service.CommentService;
 import com.trinco.researchrepo.research_repo_backend.util.mappers.CommentMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,10 +19,20 @@ public class CommentServiceIMPL implements CommentService {
     @Autowired
     private CommentRepo commentRepo;
 
+    @Autowired
+    private ProjectReviewRepo projectReviewRepo;
+
     @Override
     public String addComment(CommentSaveRequestDTO commentSaveRequestDTO) {
         Comments comments = commentMapper.RequestDtoToEntity(commentSaveRequestDTO);
         commentRepo.save(comments);
+
+        if(projectReviewRepo.existsById(comments.getProject().getProjectId())){
+            Reviews reviews = projectReviewRepo.getReferenceById(comments.getProject().getProjectId());
+            reviews.setStars(reviews.getStars() + comments.getRatingStars());
+            reviews.setRated(reviews.getRated() + 1);
+            projectReviewRepo.save(reviews);
+        }
 
         return String.valueOf(comments.getCommentId());
     }
