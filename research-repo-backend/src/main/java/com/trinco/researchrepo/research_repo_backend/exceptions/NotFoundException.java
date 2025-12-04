c1613c1349fab1601a170830cd6613b59e68ce10
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-12-05 01:32:22 +0530

    Create NotFoundException.java

---
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/exceptions/NotFoundException.java
@@ -0,0 +1,11 @@
+package com.trinco.researchrepo.research_repo_backend.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message){
+        super(message);
+    }
+}
