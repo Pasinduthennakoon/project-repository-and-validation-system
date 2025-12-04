032de5ab89b0d9b96dc3cb775ceee540c2d6e3fc
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-12-05 01:32:57 +0530

    Create CommentResponseDTO.java

---
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/dto/response/CommentResponseDTO.java
@@ -0,0 +1,15 @@
+package com.trinco.researchrepo.research_repo_backend.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CommentResponseDTO {
+    private String commentText;
+    private String createdAt; // Matches entity type
+    private int ratingStars; // Matches entity type
+    private String commenterName;
+}
