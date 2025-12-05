59e8a119d78e60e9ec7b7467e9005a386d982fd4
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-12-06 01:33:00 +0530

    Update PendingProjectServiceIMPL.java

--- research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/service/impl/PendingProjectServiceIMPL.java
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/service/impl/PendingProjectServiceIMPL.java
@@ -1,6 +1,7 @@
 package com.trinco.researchrepo.research_repo_backend.service.impl;
 
 import com.trinco.researchrepo.research_repo_backend.dto.request.PendingProjectSaveRequestDTO;
+import com.trinco.researchrepo.research_repo_backend.dto.response.PendingProjectApprovelResponseDTO;
 import com.trinco.researchrepo.research_repo_backend.entity.Pending_Projects;
 import com.trinco.researchrepo.research_repo_backend.exceptions.EntryDuplicationException;
 import com.trinco.researchrepo.research_repo_backend.repo.PendingProjectRepo;
@@ -9,6 +10,8 @@ import com.trinco.researchrepo.research_repo_backend.util.mappers.PendingProject
 import com.trinco.researchrepo.research_repo_backend.util.mappers.PendingUserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PendingProjectServiceIMPL implements PendingProjectService {
@@ -29,5 +32,13 @@ public class PendingProjectServiceIMPL implements PendingProjectService {
         }else {
             throw new EntryDuplicationException("project already exists");
         }
+    }
+
+    @Override
+    public List<PendingProjectApprovelResponseDTO> getPendingProjects() {
+        List<Pending_Projects> pendingProjects = pendingProjectRepo.findAll();
+        List<PendingProjectApprovelResponseDTO> pendingProjectApprovelResponseDTOS = pendingProjectMapper.ResponsePendingProjectEntityListToDtoList(pendingProjects);
+
+        return pendingProjectApprovelResponseDTOS;
     }
 }
