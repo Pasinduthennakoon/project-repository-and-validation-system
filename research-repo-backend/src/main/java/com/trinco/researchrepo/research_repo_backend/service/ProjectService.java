202dadafdc8afed95ee5cd4a0dccf59d90fca3be
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-12-05 13:39:43 +0530

    Update ProjectService.java

--- research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/service/ProjectService.java
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/service/ProjectService.java
@@ -1,10 +1,18 @@
 package com.trinco.researchrepo.research_repo_backend.service;
 
+import com.trinco.researchrepo.research_repo_backend.dto.LanguageUsageDTO;
 import com.trinco.researchrepo.research_repo_backend.dto.request.ProjectSaveRequestDTO;
 import com.trinco.researchrepo.research_repo_backend.dto.response.ProjectPageDataResponseDTO;
+import com.trinco.researchrepo.research_repo_backend.entity.Projects;
+
+import java.util.List;
 
 public interface ProjectService {
     String approveProject(int pendingId);
 
     ProjectPageDataResponseDTO getProjectPageData(int projectId);
+
+    Projects updateLanguageUsed(int projectId, List<LanguageUsageDTO> languageUsage);
+
+    Projects updateProjectTags(int projectId, List<String> tags);
 }
