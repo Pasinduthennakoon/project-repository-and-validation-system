30b62b3779ffb498b2c75aa2742fcacb8a90dab4
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-12-06 01:33:30 +0530

    Update PendingProjectService.java

--- research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/service/PendingProjectService.java
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/service/PendingProjectService.java
@@ -1,7 +1,17 @@
 package com.trinco.researchrepo.research_repo_backend.service;
 
 import com.trinco.researchrepo.research_repo_backend.dto.request.PendingProjectSaveRequestDTO;
+import com.trinco.researchrepo.research_repo_backend.dto.response.PendingProjectApprovelResponseDTO;
+import com.trinco.researchrepo.research_repo_backend.entity.Projects;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface PendingProjectService {
     String addPendingProject(PendingProjectSaveRequestDTO pendingProjectSaveRequestDTO);
+
+    List<PendingProjectApprovelResponseDTO> getPendingProjects();
+
 }
