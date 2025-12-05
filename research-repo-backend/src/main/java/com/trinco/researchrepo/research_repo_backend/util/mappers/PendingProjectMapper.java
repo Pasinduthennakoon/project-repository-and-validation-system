354f33d7ad3045ad822299e959a05fc4eb1aa5df
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-12-06 01:33:20 +0530

    Update PendingProjectMapper.java

--- research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/util/mappers/PendingProjectMapper.java
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/util/mappers/PendingProjectMapper.java
@@ -1,10 +1,20 @@
 package com.trinco.researchrepo.research_repo_backend.util.mappers;
 
 import com.trinco.researchrepo.research_repo_backend.dto.request.PendingProjectSaveRequestDTO;
+import com.trinco.researchrepo.research_repo_backend.dto.response.PendingProjectApprovelResponseDTO;
+import com.trinco.researchrepo.research_repo_backend.dto.response.UploadProjectUsersResponseDTO;
 import com.trinco.researchrepo.research_repo_backend.entity.Pending_Projects;
+import com.trinco.researchrepo.research_repo_backend.entity.Projects;
+import com.trinco.researchrepo.research_repo_backend.entity.Users;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface PendingProjectMapper {
     Pending_Projects RequestDtoToEntity(PendingProjectSaveRequestDTO pendingProjectSaveRequestDTO);
+
+    List<UploadProjectUsersResponseDTO> ResponseUsersEntityListToDtoList(List<Users> users);
+
+    List<PendingProjectApprovelResponseDTO> ResponsePendingProjectEntityListToDtoList(List<Pending_Projects> pendingProjects);
 }
