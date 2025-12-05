090ed4d6c4cd34e5e287853cb4e3ef6b6bb2a239
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-12-06 01:31:50 +0530

    Update CommentController.java

--- research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/controller/CommentController.java
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/controller/CommentController.java
@@ -1,12 +1,16 @@
 package com.trinco.researchrepo.research_repo_backend.controller;
 
 import com.trinco.researchrepo.research_repo_backend.dto.request.CommentSaveRequestDTO;
+import com.trinco.researchrepo.research_repo_backend.dto.response.ProjectReviewResponseDTO;
 import com.trinco.researchrepo.research_repo_backend.service.CommentService;
+import com.trinco.researchrepo.research_repo_backend.service.ProjectService;
 import com.trinco.researchrepo.research_repo_backend.util.StandardResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/project/comment")
@@ -15,6 +19,9 @@ public class CommentController {
 
     @Autowired
     private CommentService commentService;
+
+    @Autowired
+    private ProjectService projectService;
 
 //add comment(supervisor)
     @PostMapping(
@@ -27,6 +34,19 @@ public class CommentController {
         return new ResponseEntity<StandardResponse>(
                 new StandardResponse(201, "Add Comment Successfully",projectId),
                 HttpStatus.CREATED
+        );
+    }
+
+//review projects(supervisor review)
+    @GetMapping(
+            path = {"/view_projects"}
+    )
+    public ResponseEntity<StandardResponse> viewProjectForReview(){
+        List<ProjectReviewResponseDTO> projectReviewResponseDTOS = projectService.viewProjectForReview();
+
+        return new ResponseEntity<StandardResponse>(
+                new StandardResponse(201, "success", projectReviewResponseDTOS),
+                HttpStatus.OK
         );
     }
 }
