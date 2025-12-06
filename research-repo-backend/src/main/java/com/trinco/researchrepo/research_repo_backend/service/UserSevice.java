2b3be5dc4694c9aa67349d09291bf9ba7bd27a75
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-12-06 12:47:42 +0530

    Update UserSevice.java

--- research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/service/UserSevice.java
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/service/UserSevice.java
@@ -2,8 +2,11 @@ package com.trinco.researchrepo.research_repo_backend.service;
 
 import com.trinco.researchrepo.research_repo_backend.dto.request.PendingUserSaveRequestDTO;
 import com.trinco.researchrepo.research_repo_backend.dto.request.UserSaveRequestDTO;
+import com.trinco.researchrepo.research_repo_backend.dto.response.UploadProjectUsersResponseDTO;
 import com.trinco.researchrepo.research_repo_backend.entity.Users;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 public interface UserSevice {
     Users addUser(UserSaveRequestDTO userSaveRequestDTO);
@@ -11,4 +14,6 @@ public interface UserSevice {
     String approveUser(int pendingId);
 
     String updateActiveState(int userId, boolean status);
+
+    List<UploadProjectUsersResponseDTO> uploadUses();
 }
