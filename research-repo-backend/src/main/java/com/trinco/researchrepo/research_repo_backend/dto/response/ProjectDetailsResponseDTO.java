72114be0cfa0c8fca7e1ac404b4f13aca734f76a
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-12-07 03:22:32 +0530

    Update ProjectDetailsResponseDTO.java

--- research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/dto/response/ProjectDetailsResponseDTO.java
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/dto/response/ProjectDetailsResponseDTO.java
@@ -1,6 +1,8 @@
 package com.trinco.researchrepo.research_repo_backend.dto.response;
 
 import com.trinco.researchrepo.research_repo_backend.dto.LanguageUsageDTO;
+import com.trinco.researchrepo.research_repo_backend.dto.StudentDTO;
+import com.trinco.researchrepo.research_repo_backend.dto.SupervisorDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,17 +23,8 @@ public class ProjectDetailsResponseDTO {
     private List<String> tags; // Using List<String> based on entity
     private List<LanguageUsageDTO> languagesUsed; // Using List<LanguageUsageDTO> based on entity
 
-    // Uploader Info (from Users & Students)
-    private String uploaderName;
-    private String uploaderPhoto;
-    private String uploaderBatch;
-    private String uploaderDepartment;
-
-    // Supervisor Info (from Users)
-    private String supervisorName;
-    private String supervisorPhoto;
-    private String supervisorEmail;
-    private String supervisorDepartment;
+    private StudentDTO students;
+    private SupervisorDTO supervisors;
 
     // Review Info (from Reviews)
     private int watches; // Changed to int based on entity
