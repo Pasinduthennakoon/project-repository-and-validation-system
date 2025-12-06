e43a6e287c97291cd0053f73695c1ae9921955c7
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-12-06 12:47:54 +0530

    Update PendingUserService.java

--- research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/service/PendingUserService.java
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/service/PendingUserService.java
@@ -1,8 +1,12 @@
 package com.trinco.researchrepo.research_repo_backend.service;
 
 import com.trinco.researchrepo.research_repo_backend.dto.request.PendingUserSaveRequestDTO;
+import com.trinco.researchrepo.research_repo_backend.dto.response.PendingUsersResponseDTO;
+
+import java.util.List;
 
 public interface PendingUserService {
     String addPendingUser(PendingUserSaveRequestDTO pendingUserSaveRequestDTO);
 
+    List<PendingUsersResponseDTO> getPendingUsersForApproval();
 }
