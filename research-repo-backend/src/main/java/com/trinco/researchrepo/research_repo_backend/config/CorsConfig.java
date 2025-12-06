64c4537ad6103de3f382d4df51b510d136c86733
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-12-06 22:01:56 +0530

    Create CorsConfig.java

---
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/config/CorsConfig.java
@@ -0,0 +1,16 @@
+package com.trinco.researchrepo.research_repo_backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // Apply to all endpoints starting with /api
+                .allowedOrigins("http://localhost:3000") // The origin of your React app
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Specify allowed HTTP methods
+                .allowedHeaders("*"); // Allow all headers
+    }
+}
