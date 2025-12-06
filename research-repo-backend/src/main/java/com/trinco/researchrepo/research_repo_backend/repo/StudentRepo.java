4672ce9de9a967ac016a8506570906cc8bcd3692
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-12-06 12:48:15 +0530

    Update StudentRepo.java

--- research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/repo/StudentRepo.java
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/repo/StudentRepo.java
@@ -3,11 +3,19 @@ package com.trinco.researchrepo.research_repo_backend.repo;
 import com.trinco.researchrepo.research_repo_backend.entity.Students;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @EnableJpaRepositories
 @Repository
 @Transactional
 public interface StudentRepo extends JpaRepository<Students, Integer> {
+
+    @Query("""
+        SELECT s FROM Students s
+        WHERE s.regNo = :regNo
+    """)
+    Students findByRegNo(@Param("regNo") String regNo);
 }
