1eb4b588a89f0132a5a7587c1b643e12d577f1e7
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-12-06 22:03:58 +0530

    Update PendingProjectServiceIMPL.java

--- research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/service/impl/PendingProjectServiceIMPL.java
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/service/impl/PendingProjectServiceIMPL.java
@@ -1,17 +1,23 @@
 package com.trinco.researchrepo.research_repo_backend.service.impl;
 
 import com.trinco.researchrepo.research_repo_backend.dto.request.PendingProjectSaveRequestDTO;
+import com.trinco.researchrepo.research_repo_backend.dto.request.ProjectSaveRequestDTO;
 import com.trinco.researchrepo.research_repo_backend.dto.response.PendingProjectApprovelResponseDTO;
-import com.trinco.researchrepo.research_repo_backend.entity.Pending_Projects;
+import com.trinco.researchrepo.research_repo_backend.entity.*;
 import com.trinco.researchrepo.research_repo_backend.exceptions.EntryDuplicationException;
-import com.trinco.researchrepo.research_repo_backend.repo.PendingProjectRepo;
+import com.trinco.researchrepo.research_repo_backend.repo.*;
+import com.trinco.researchrepo.research_repo_backend.service.GoogleDriveService;
 import com.trinco.researchrepo.research_repo_backend.service.PendingProjectService;
 import com.trinco.researchrepo.research_repo_backend.util.mappers.PendingProjectMapper;
 import com.trinco.researchrepo.research_repo_backend.util.mappers.PendingUserMapper;
+import com.trinco.researchrepo.research_repo_backend.util.mappers.ProjectMapper;
+import jakarta.el.PropertyNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PendingProjectServiceIMPL implements PendingProjectService {
@@ -21,6 +27,24 @@ public class PendingProjectServiceIMPL implements PendingProjectService {
 
     @Autowired
     private PendingProjectRepo pendingProjectRepo;
+
+    @Autowired
+    private StudentRepo studentRepo;
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private ProjectMapper projectMapper;
+
+    @Autowired
+    private GoogleDriveService googleDriveService;
+
+    @Autowired
+    private ProjectRepo projectRepo;
+
+    @Autowired
+    private ProjectReviewRepo projectReviewRepo;
 
     @Override
     public String addPendingProject(PendingProjectSaveRequestDTO pendingProjectSaveRequestDTO) {
@@ -40,5 +64,58 @@ public class PendingProjectServiceIMPL implements PendingProjectService {
         List<PendingProjectApprovelResponseDTO> pendingProjectApprovelResponseDTOS = pendingProjectMapper.ResponsePendingProjectEntityListToDtoList(pendingProjects);
 
         return pendingProjectApprovelResponseDTOS;
+    }
+
+    @Override
+    public String approveProject(int pendingId) {
+
+        Pending_Projects pending_projects = pendingProjectRepo.findById(pendingId)
+                .orElseThrow(() -> new RuntimeException("pending project not found"));
+
+        String regNo = pending_projects.getRegNo();
+        Students student = studentRepo.findByRegNo(regNo);
+        Optional<Users> uploader = userRepo.findById(student.getUserId());
+
+        PendingProjectSaveRequestDTO pendingProjectSaveRequestDTO = projectMapper.EntityToDtoToAddProject(pending_projects);
+        ProjectSaveRequestDTO projectSaveRequestDTO = projectMapper.PendingProjectsToProjects(pendingProjectSaveRequestDTO);
+        Projects projects = projectMapper.RequestDtoToEntity(projectSaveRequestDTO);
+        projects.setUploader(uploader.get());
+
+
+        File pdfFile = new File(pending_projects.getTempPdfPath());
+
+        try {
+
+            String googleDriveUrl = googleDriveService.uploadFileToDrive(pdfFile);
+
+            projects.setPdfLink(googleDriveUrl);
+            projectRepo.save(projects);
+
+            Reviews reviews = new Reviews();
+            reviews.setProject(projects);
+            projectReviewRepo.save(reviews);
+
+
+            // 3. Delete temp file
+            if(pdfFile.exists()) pdfFile.delete();
+
+            // 4. Remove pending project row
+            pendingProjectRepo.delete(pending_projects);
+
+            return String.valueOf(projects.getProjectId());
+
+        } catch (Exception e) {
+            throw new RuntimeException("Project approval failed due to file or database error.", e);
+        }
+    }
+
+    @Override
+    public boolean rejectPendingProject(int pendingProjectId) {
+        if(pendingProjectRepo.existsById(pendingProjectId)) {
+            pendingProjectRepo.deleteById(pendingProjectId);
+        }else {
+            throw new PropertyNotFoundException("Not found project for this id");
+        }
+        return true;
     }
 }
