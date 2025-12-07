cd1475514dfa4b622f5c2232c6d4fcc789b8f217
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-12-07 23:38:56 +0530

    Create LoginRequestDTO.java

---
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/dto/request/LoginRequestDTO.java
@@ -0,0 +1,13 @@
+package com.trinco.researchrepo.research_repo_backend.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class LoginRequestDTO {
+    private String email;
+    private String password;
+}
