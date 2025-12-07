8d9546bc56d5891ee48565a86ccc065100538093
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-12-07 23:38:34 +0530

    Update CorsConfig.java

--- research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/config/CorsConfig.java
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/config/CorsConfig.java
@@ -1,11 +1,15 @@
 package com.trinco.researchrepo.research_repo_backend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Apply to all endpoints starting with /api
