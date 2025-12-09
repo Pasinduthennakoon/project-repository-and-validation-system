162ff925994d6028add23d2ca54cc7ae085a2518
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-12-09 23:02:06 +0530

    Update PendingUserMapper.java

--- research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/util/mappers/PendingUserMapper.java
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/util/mappers/PendingUserMapper.java
@@ -4,11 +4,14 @@ import com.trinco.researchrepo.research_repo_backend.dto.request.PendingUserSave
 import com.trinco.researchrepo.research_repo_backend.dto.response.PendingUsersResponseDTO;
 import com.trinco.researchrepo.research_repo_backend.entity.Pending_Users;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface PendingUserMapper {
+
+    @Mapping(target = "password", ignore = true)
     Pending_Users RequestToDtoToEntity(PendingUserSaveRequestDTO pendingUserSaveRequestDTO);
 
     List<PendingUsersResponseDTO> ResponseEntityListToDtoList(List<Pending_Users> pendingUsers);
