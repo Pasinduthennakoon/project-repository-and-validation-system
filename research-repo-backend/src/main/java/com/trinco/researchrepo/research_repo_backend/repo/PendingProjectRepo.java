97cfb58e7b0af8782275ffa3f9ef8cb2e170f5a2
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-12-12 02:39:29 +0530

    Update PendingProjectRepo.java

--- research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/repo/PendingProjectRepo.java
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/repo/PendingProjectRepo.java
@@ -3,11 +3,28 @@ package com.trinco.researchrepo.research_repo_backend.repo;
 import com.trinco.researchrepo.research_repo_backend.entity.Pending_Projects;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.method.P;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @EnableJpaRepositories
 @Repository
 @Transactional
 public interface PendingProjectRepo extends JpaRepository<Pending_Projects, Integer> {
+
+    @Query("""
+        SELECT p FROM Pending_Projects p
+        WHERE p.supervisorId = :supervisorId
+""")
+    List<Pending_Projects> findBySupervisorId(@Param("supervisorId") int supervisorId);
+
+    @Query("""
+        SELECT p.tempPdfPath FROM Pending_Projects p
+        WHERE p.pendingProjectId = :pendingProjectId
+""")
+    String gettempPdfPath(@Param("pendingProjectId") int pendingProjectId);
 }
