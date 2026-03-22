c189ebd133123298ba62a24784476415c5c5e19b
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2026-03-22 15:34:32 +0530

    Create AnalysisIdeaServiceIMPL.java

---
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/service/impl/AnalysisIdeaServiceIMPL.java
@@ -0,0 +1,34 @@
+package com.trinco.researchrepo.research_repo_backend.service.impl;
+
+import org.checkerframework.checker.units.qual.A;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.trinco.researchrepo.research_repo_backend.dto.request.AnalysisIdeaRequestDTO;
+import com.trinco.researchrepo.research_repo_backend.entity.Analysed_Idea;
+import com.trinco.researchrepo.research_repo_backend.exceptions.EntryDuplicationException;
+import com.trinco.researchrepo.research_repo_backend.repo.AnalysisIdeaRepo;
+import com.trinco.researchrepo.research_repo_backend.service.AnalysisIdeaService;
+import com.trinco.researchrepo.research_repo_backend.util.mappers.AnalysisIdeaMapper;
+
+@Service
+public class AnalysisIdeaServiceIMPL implements AnalysisIdeaService {
+
+    @Autowired
+    private AnalysisIdeaRepo analysisIdeaRepo;
+
+    @Autowired
+    private AnalysisIdeaMapper analysisIdeaMapper;
+
+    @Override
+    public String saveIdea(AnalysisIdeaRequestDTO analysisIdeaRequestDTO) {
+        Analysed_Idea analysedIdea = analysisIdeaMapper.RequestDtoToEntity(analysisIdeaRequestDTO);
+        if(!analysisIdeaRepo.existsById(analysedIdea.getIdeaId())){
+            analysisIdeaRepo.save(analysedIdea);
+            return "successfully saved the idea";
+        }else {
+            throw new EntryDuplicationException("Idea already exists");
+        }
+    }
+    
+}
