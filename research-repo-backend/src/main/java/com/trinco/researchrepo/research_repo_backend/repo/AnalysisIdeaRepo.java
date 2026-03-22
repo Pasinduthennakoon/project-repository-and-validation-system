c42dae922ba4c602019ea4944d97839447a7122d
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2026-03-22 15:33:51 +0530

    Create AnalysisIdeaRepo.java

---
+++ research-repo-backend/src/main/java/com/trinco/researchrepo/research_repo_backend/repo/AnalysisIdeaRepo.java
@@ -0,0 +1,14 @@
+package com.trinco.researchrepo.research_repo_backend.repo;
+
+import com.trinco.researchrepo.research_repo_backend.entity.Analysed_Idea;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+@EnableJpaRepositories
+@Repository
+@Transactional
+public interface AnalysisIdeaRepo extends JpaRepository<Analysed_Idea, Integer> {
+}
+
